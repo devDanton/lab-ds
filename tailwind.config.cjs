94529698d8fd65e72748961c1a2c2fea240eb044
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2022-10-23 23:52:57 -0300

    Config init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./scrc/**/*.tsx'],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {}
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1E6',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#9BE1FB'
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
   },
   plugins: []
 }
